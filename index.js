812072e19173910745bb8e2f504df7ebc5b364b6
Alexander Shutilov <avshutilov@yandex.ru>
2020-09-23 12:03:42 +0300

    add export options

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
     process.env.OUTPUT_PATH = core.getInput("output-path");
     process.env.SCHEME = core.getInput("scheme");
     process.env.DISABLE_TARGETS = core.getInput("disable-targets");
+    process.env.EXPORT_OPTIONS = core.getInput("export-options");
     await exec.exec(`bash ${__dirname}/build.sh`);
   } catch (error) {
     core.setFailed(error.message);
